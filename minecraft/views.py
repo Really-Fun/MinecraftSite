e3b4c2410467ec3f813310070f630697c74952d6
ReallyChubrik <ilyareztcov@gmail.com>
2024-08-09 16:21:03 +0300

    add online from queryclient

--- minecraft/views.py
+++ minecraft/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import ListView, TemplateView
 from minecraft.models import News
+from minecraft.service import get_online_servers
 
 
 # Create your views here.
 class HomePage(ListView):
     template_name = "minecraft/index.html"
-    extra_context = {"title": "AdventTime"}
+    extra_context = {
+        "title": "AdventTime",
+        "servers": get_online_servers([("209.222.115.129", "25565")]),
+    }
     allow_empty = True
     model = News
     context_object_name = "news"
